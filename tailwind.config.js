5bb83164971abf2090fc53a7d805eba31a0aba66
ArunKumar_D <arunkumard1018@gmail.com>
2024-01-07 13:49:52 +0530

    adding the spotify clone app

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#121212',
+        customBlackRightHeader : '#101010',
+        customBgPaylist : '#242424',
+        customBgMainPaylist : '#1e1e1e',
+        customBgPaylistCard : '#181818',
+        customSocialBg : '#292929',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
